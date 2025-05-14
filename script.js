07cb53653fba5cacfbbfc45130b27a6987edd26f
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-14 18:08:49 +0700

    Create a function that ask for user input

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// Write the logic to return the computer choice
-// randomly return "rock", "paper" or "scissors"
+// Function that return the computer choice
+// randomly return 0-2 as a "rock", "paper" or "scissors"
 function getComputerChoice() {
     // Get a random number from 0 1 2 
     return Math.floor(Math.random() * 3);
 }
 
 console.log(getComputerChoice());
+
+// Function that take user input and turn into number
+function getHumanChoice() {
+    let choice = prompt('rock, paper, or scissors?')
+}
+
+getHumanChoice();
