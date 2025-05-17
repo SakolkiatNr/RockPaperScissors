dac4641fd8fd64aefd2a4bf1ec8b34826f81fb1f
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-17 22:07:07 +0700

    Create buttons container

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function getComputerChoice() {
     // Get a random number from 0 1 2 
     return Math.floor(Math.random() * 3);
 }
-// console.log(getComputerChoice());
 
 // Function that take user input and turn into number
 function getHumanChoice() {
@@ -88,6 +87,12 @@ function playGame() {
             }
         }
     }
+
+    const rockButt = document.querySelector('#rock');
+    const paperButt = document.querySelector('#paper');
+    const scissorsButt = document.querySelector('#scissors');
+
+    console.log(rockButt);
 
     // // Play 5 rounds
     // for (let i = 0; i < 5; i++) {
